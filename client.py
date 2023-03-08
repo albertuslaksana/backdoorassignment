e91e47ce2b9bbc756f129999f6b3fa3bf5cd0973
albertuslaksana <89668012+albertuslaksana@users.noreply.github.com>
2023-03-08 10:34:19 -0600

    Update client.py

--- client.py
+++ client.py
@@ -3,13 +3,23 @@ import subprocess
 
 REMOTE_HOST = '10.0.2.5'
 REMOTE_PORT = 4444
+IN_SHELL = False
+PASSWORD = ""
 client = socket.socket()
-print("[-] Connection Initiating...")
-client.connect((REMOTE_HOST, REMOTE_PORT))
-print("[-] Connection initiated!")
+print("Starting Connection")
+input_password = input('Enter Password: ')
+if input_password == PASSWORD:
+    IN_SHELL = True
+    client.connect((REMOTE_HOST, REMOTE_PORT)) 
+    print("Connected!")
+else:
+    print("Incorrect Password")
 
-while True:
-    command = input('Enter Command : ')
+while IN_SHELL == True:
+    command = input('Enter Command: ')
+    if command == "exit":
+        IN_SHELL = False
+        break;
     command = command.encode()
     client.send(command)
     print('[+] Command sent')
